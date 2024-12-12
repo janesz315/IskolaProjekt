7759800dd3b326ce9e6906891fd43d68bdfb9549
janesz315 <borosuveg6@gmail.com>
2024-12-12 16:30:05 +0100

    paginátor kész

--- frontend/src/components/Paginator.vue
+++ frontend/src/components/Paginator.vue
@@ -1,24 +1,50 @@
 <template>
   <nav aria-label="...">
     <ul class="pagination">
-      <li class="page-item disabled">
-        <a class="page-link">Previous</a>
+      <li class="page-item" :class="{ disabled: isPreviousDisabled }">
+        <a class="page-link" href="#" @click.prevent="goToPreviousPage"
+          >Previous</a
+        >
       </li>
-      <li class="page-item" v-for="number in pagenumbers" :key="number">
-        <a class="page-link" href="#">{{ number }}</a>
+      <li
+        class="page-item"
+        v-for="number in pagenumbers"
+        :key="number"
+        :class="{ active: currentPage === number }"
+      >
+        <a class="page-link" href="#" @click.prevent="goToPage(number)">{{
+          number
+        }}</a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#">Next</a>
+      <li class="page-item" :class="{ disabled: isNextDisabled }">
+        <a class="page-link" href="#" @click.prevent="goToNextPage">Next</a>
       </li>
     </ul>
   </nav>
 </template>
-
-<script>
+  
+  <script>
 export default {
-  props: ["pagenumbers"],
+  props: [
+    "pagenumbers",
+    "currentPage",
+    "totalPages",
+    "isPreviousDisabled",
+    "isNextDisabled",
+  ],
+  methods: {
+    goToPage(page) {
+      this.$emit("changePage", page); // Emit page-changed event to the parent component
+    },
+    goToPreviousPage() {
+      this.$emit("previousPage"); // Emit previous-page event to the parent component
+    },
+    goToNextPage() {
+      this.$emit("nextPage"); // Emit next-page event to the parent component
+    },
+  },
 };
 </script>
-
-<style>
+  
+  <style scoped>
 </style>
--- frontend/src/views/KartyakView.vue
+++ frontend/src/views/KartyakView.vue
@@ -2,7 +2,9 @@
   <div>
     <div class="d-flex justify-content-between align-items-center">
       <h1>Cards</h1>
-      <!-- We choose how many cards we want to see -->
+      <!-- Dropdown to select how many cards to display per page -->
+      <!-- v-model: Data binding to the howManyCards variable -->
+      <!-- @change: Trigger change when a new value is selected  -->
       <select
         class="form-select"
         style="width: 150px"
@@ -10,74 +12,124 @@
         v-model="howManyCards"
         @change="handleCardsChange"
       >
+        <!-- Loop through the array of options -->
         <option
           v-for="(number, index) in howManyCardsArray"
           :key="index"
           :value="number"
         >
           {{ number }}
+          <!-- Display each option value -->
         </option>
       </select>
     </div>
-    <!-- Visualising the cards with the component -->
+
+    <!-- Display the cards on the page -->
     <div class="my-cards-height overflow-y-auto">
       <Cards :cards="cards"></Cards>
     </div>
 
-    <!-- Pagination -->
+    <!-- Pagination Component -->
+
+    <!-- pagenumbers: Pass the page numbers array to the Paginator -->
+    <!-- currentPage: Pass the current page to the Paginator -->
+    <!-- @changePage: Listen for page change events from the Paginator -->
+    <!-- @previousPage: Listen for 'Previous' page change -->
+    <!-- @nextPage: Listen for 'Next' page change -->
+    <!-- isPreviousDisabled: Disable 'Previous' button if necessary -->
+    <!-- isNextDisabled: Disable 'Next' button if necessary -->
     <div class="p-2">
-      <Paginator :pagenumbers="pagenumbersArray"></Paginator>
+      <Paginator
+        :pagenumbers="pagenumbersArray"
+        :currentPage="currentPage"
+        :totalPages="totalPages"
+        @changePage="handlePageChange"
+        @previousPage="handlePreviousPage"
+        @nextPage="handleNextPage"
+        :isPreviousDisabled="currentPage === 1"
+        :isNextDisabled="currentPage === totalPages"
+      ></Paginator>
     </div>
   </div>
 </template>
-
-<script>
+  
+  <script>
 import axios from "axios";
-import Cards from "@/components/Cards.vue";
-import Paginator from "@/components/Paginator.vue";
+import Cards from "@/components/Cards.vue"; //Import the Cards component
+import Paginator from "@/components/Paginator.vue"; // Import the Paginator component
 export default {
   components: { Cards, Paginator },
   data() {
     return {
-      urlApi: "http://localhost:8000/api",
-      cards: [],
-      currentPage: 1,
-      howManyCards: 3,
-      howManyCardsArray: [1, 2, 3, 4, 5, 6, 7, 10, 20],
-      pagenumbersArray: [],
+      urlApi: "http://localhost:8000/api", // Base URL for the API
+      cards: [], // Array to hold the card data fetched from the API
+      currentPage: 1, // The current page that is displayed
+      howManyCards: 3, // The number of cards displayed per page
+      howManyCardsArray: [1, 2, 3, 4, 5, 6, 7, 10, 20], // The available options for how many cards per page
+      pagenumbersArray: [], // Array to store the page numbers for pagination
+      totalPages: 1, // Total number of pages (calculated based on the data)
     };
   },
+  // Fetch data when the component is mounted
   async mounted() {
-    await this.getClassRoster();
-    await this.getPageNumbers();
+    await this.getClassRoster(); // Get the cards data
+    await this.getPageNumbers(); // Get the total number of pages
   },
   methods: {
+    // Fetch the list of cards based on the current page and number of cards per page
     async getClassRoster() {
       const url = `${this.urlApi}/queryOsztalynevsorLimit/${this.currentPage}/${this.howManyCards}`;
-      const response = await axios.get(url);
-      this.cards = response.data.data;
-      console.log(this.cards);
+      const response = await axios.get(url); // Make a GET request to fetch data
+      this.cards = response.data.data; // Assign the fetched data to the 'cards' array
     },
-
+    // Fetch the total number of pages based on how many cards are displayed per page
     async getPageNumbers() {
       const url = `${this.urlApi}/queryHanyOldalVan/${this.howManyCards}`;
-      const response = await axios.get(url);
-      const totalPages = response.data.data.oldalszam;
-      this.pagenumbersArray = [];
-      for (let i = 0; i < totalPages; i++) {
-        this.pagenumbersArray.push(i + 1);
+      const response = await axios.get(url); // Make a GET request to fetch the page number data
+      this.totalPages = response.data.data.oldalszam; // Assign total number of pages to totalPages
+      this.pagenumbersArray = []; // Reset the page numbers array
+      // Populate the page numbers array with the available page numbers
+      for (let i = 0; i < this.totalPages; i++) {
+        this.pagenumbersArray.push(i + 1); // Add page numbers starting from 1
+      }
+
+      // If the current page exceeds the total pages, adjust to the last available page
+      if (this.currentPage > this.totalPages) {
+        this.currentPage = this.totalPages > 0 ? this.totalPages : 1; // Set the current page to the last available page or 1 if no pages
+      }
+      this.getClassRoster(); // Fetch the cards again with the updated page
+    },
+    // Handle changes in the number of cards per page
+    handleCardsChange() {
+      this.getPageNumbers(); // Recalculate page numbers based on new selection
+    },
+    // Handle when a user clicks on a page number
+    handlePageChange(page) {
+      if (page !== this.currentPage) {
+        this.currentPage = page; // Update the current page
+        this.getClassRoster(); // Fetch the cards for the new page
       }
     },
-    handleCardsChange() {
-      this.getClassRoster();  // Fetch new cards based on selected number
-      this.getPageNumbers();  // Recalculate page numbers based on new selection
+    // Handle the "Previous" button click
+    handlePreviousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--; // Go to the previous page
+        this.getClassRoster(); // Fetch the cards for the previous page
+      }
+    },
+    // Handle the "Next" button click
+    handleNextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++; // Go to the next page
+        this.getClassRoster(); // Fetch the cards for the next page
+      }
     },
   },
 };
 </script>
-
-<style>
+  
+  <style>
 .my-cards-height {
-  height: calc(100vh - 300px);
+  height: calc(100vh - 300px); /* Set height for the card container */
 }
 </style>
